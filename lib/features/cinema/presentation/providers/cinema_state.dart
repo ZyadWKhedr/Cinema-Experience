83e3cb4fb64fe1bb1cd00eae0c2cd5843d9547cb
Zyad <ziad.w.khedr@gmail.com>
2026-06-06 15:31:56 +0300

    feat: add cinema state model

---
+++ lib/features/cinema/presentation/providers/cinema_state.dart
@@ -0,0 +1,56 @@
+import 'package:equatable/equatable.dart';
+
+class CinemaState extends Equatable {
+  final int countdown;
+
+  final String? selectedSeatId;
+
+  final bool isConfirmed;
+
+  final bool showTicket;
+
+  final bool showThankYou;
+
+  const CinemaState({
+    required this.countdown,
+    required this.selectedSeatId,
+    required this.isConfirmed,
+    required this.showTicket,
+    required this.showThankYou,
+  });
+
+  factory CinemaState.initial() {
+    return const CinemaState(
+      countdown: 3,
+      selectedSeatId: null,
+      isConfirmed: false,
+      showTicket: false,
+      showThankYou: false,
+    );
+  }
+
+  CinemaState copyWith({
+    int? countdown,
+    String? selectedSeatId,
+    bool? isConfirmed,
+    bool? showTicket,
+    bool? showThankYou,
+  }) {
+    return CinemaState(
+      countdown: countdown ?? this.countdown,
+      selectedSeatId: selectedSeatId ?? this.selectedSeatId,
+      isConfirmed: isConfirmed ?? this.isConfirmed,
+      showTicket: showTicket ?? this.showTicket,
+      showThankYou: showThankYou ?? this.showThankYou,
+    );
+  }
+
+  @override
+  List<Object?> get props => [
+        countdown,
+        selectedSeatId,
+        isConfirmed,
+        showTicket,
+        showThankYou,
+      ];
+}
