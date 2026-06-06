eaa8ae985c590779b17c5f02ffb6af75ad712716
Zyad <ziad.w.khedr@gmail.com>
2026-06-06 15:35:28 +0300

    feat: add cinema provider and seat selection logic

---
+++ lib/features/cinema/presentation/providers/cinema_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'cinema_state.dart';
+
+final cinemaProvider =
+    NotifierProvider<CinemaController, CinemaState>(
+  CinemaController.new,
+);
+
+class CinemaController extends Notifier<CinemaState> {
+  @override
+  CinemaState build() {
+    return CinemaState.initial();
+  }
+
+  void selectSeat(String seatId) {
+    state = state.copyWith(
+      selectedSeatId: seatId,
+    );
+  }
+}
