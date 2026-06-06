8666fda4d90902f9410401efc07243c2dbbf42ff
Zyad <ziad.w.khedr@gmail.com>
2026-06-06 15:38:28 +0300

    feat: connect cinema provider to ui

--- lib/features/cinema/presentation/pages/cinema_page.dart
+++ lib/features/cinema/presentation/pages/cinema_page.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CinemaPage extends StatelessWidget {
+import '../providers/cinema_provider.dart';
+
+class CinemaPage extends ConsumerWidget {
   const CinemaPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SizedBox.expand(),
-    );
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final state = ref.watch(cinemaProvider);
+
+   return Scaffold(
+  body: Center(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          state.selectedSeatId ?? 'No Seat Selected',
+        ),
+
+        const SizedBox(height: 20),
+
+        ElevatedButton(
+          onPressed: () {
+            ref
+                .read(cinemaProvider.notifier)
+                .selectSeat('A5');
+          },
+          child: const Text(
+            'Select A5',
+          ),
+        ),
+      ],
+    ),
+  ),
+);
   }
 }
